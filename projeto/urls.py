a02f607d10b1dae26a94a5a6e00b2c86ba35c118
Gustavo castro <gustavonascimentocastro1@gmail.com>
2025-01-28 20:02:55 -0300

    exemplo de templates

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('recipes.urls')), # nesse caso, caso tenha varios proj dominio.com/nome/urls_do_arquivo
 ]
